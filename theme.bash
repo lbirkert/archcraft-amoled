873a312055b0d21ce326b04eb2b99c8f26738c86
Lucas Birkert <lucasbirkert@gmail.com>
2023-11-02 20:36:40 +0100

    Increase gap & round more

--- theme.bash
+++ theme.bash
@@ -65,7 +65,7 @@ dunst_separator='2'
 
 # Picom
 picom_backend='glx'
-picom_corner='8'
+picom_corner='10'
 picom_shadow_r='24'
 picom_shadow_o='0.30'
 picom_shadow_x='-12'
@@ -79,5 +79,5 @@ bspwm_nbc="$background"
 bspwm_abc="$color5"
 bspwm_pfc="$color2"
 bspwm_border='0'
-bspwm_gap='8'
+bspwm_gap='10'
 bspwm_sratio='0.50'
